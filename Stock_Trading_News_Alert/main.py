a5c2c1ff4fd98c541a24c9ec5ce8e0f51b67b983
Raed Hasan <136272243+RaedHasan1@users.noreply.github.com>
2023-07-07 20:28:39 +0300

    main.py

--- Stock_Trading_News_Alert/main.py
+++ Stock_Trading_News_Alert/main.py
@@ -1,25 +1,8 @@
 import requests
 import smtplib
+import KEYS
 
-sender = "sender@gmail.com"
-password = "password"
-receivers = "receivers@gmail.com"
-
-STOCK_API_KEY = "STOCK_API_KEY"
-STOCK_NAME = "TSLA"
-COMPANY_NAME = "Tesla Inc"
-DIFF_PERCENTAGE = 4
-
-NEWS_API_KEY = "NEWS_API_KEY"
-
-stock_params = {
-    "function": "TIME_SERIES_DAILY_ADJUSTED",
-    "symbol": STOCK_NAME,
-    "apikey": STOCK_API_KEY,
-}
-
-STOCK_ENDPOINT = "https://www.alphavantage.co/query"
-stock_request = requests.get(STOCK_ENDPOINT, params=stock_params)
+stock_request = requests.get(KEYS.STOCK_ENDPOINT, params=KEYS.stock_params)
 stock_request.raise_for_status()
 stock_data = stock_request.json()["Time Series (Daily)"]
 stock_data_list = [value for (key, value) in stock_data.items()]
@@ -30,31 +13,25 @@ two_days_stock_data = stock_data_list[1]
 two_days_stock_closing_data = two_days_stock_data["4. close"]
 
 difference = abs(float(yesterday_stock_closing_data) - float(two_days_stock_closing_data))
+diff_sign = "🔺"
 if (float(yesterday_stock_closing_data) - float(two_days_stock_closing_data)) <= 0:
     diff_sign = "🔻"
-elif float(yesterday_stock_closing_data) - float(two_days_stock_closing_data) > 0:
-    diff_sign = "🔺"
+
 diff_percent = (difference / float(two_days_stock_closing_data)) * 100
 
-news_params = {
-    "apiKey": NEWS_API_KEY,
-    "q": COMPANY_NAME,
-    "sortBy": "popularity"
-}
-
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
-news_request = requests.get(NEWS_ENDPOINT, params=news_params)
+news_request = requests.get(NEWS_ENDPOINT, params=KEYS.news_params)
 news_request.raise_for_status()
 five_articles = news_request.json()["articles"][:5]
 five_articles_list = [f"HeadLine: {article['title']} \nBrief: {article['description']}" for article in five_articles]
 
-if diff_percent > DIFF_PERCENTAGE:
+if diff_percent > KEYS.DIFF_PERCENTAGE:
     for article in five_articles_list:
         with smtplib.SMTP("smtp.gmail.com") as connection:
             connection.starttls()  # safe connection
-            connection.login(user=sender, password=password)
+            connection.login(user=KEYS.sender, password=KEYS.password)
             connection.sendmail(
-                from_addr=sender,
-                to_addrs=receivers,
+                from_addr=KEYS.sender,
+                to_addrs=KEYS.receivers,
                 msg=f"{diff_sign}{diff_percent}\n{article}"
             )
